160be83c993a0b879c83c9503fcbeec7b6cfb76a
kmannju <61678094+kmannju@users.noreply.github.com>
2020-03-23 13:50:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+  import React from 'react';
 import logo from './logo.svg';
 import './App.css'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
@@ -11,6 +11,7 @@ function App() {
     <Router>
       <div className="App">
         <Route path="/" exact component={World} />
+        <Route path="/" component={World} />
          <Route path="/country/:country" component={Country} />
       </div>
     </Router>
